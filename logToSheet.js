7becbc807283939df577afbca97eedb6ccb8ada2
Jack <jackrains@pm.me>
2023-01-21 13:00:17 +0100

    initial commit

---
+++ logToSheet.js
@@ -0,0 +1,25 @@
+class LogToSheet {
+  constructor(options) {
+    this.sheetName = options.sheet;
+    this.sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(this.sheetName);
+    this.logs = [];
+  }
+
+  insert(log) {
+    if(log) {
+      let timestamp = Utilities.formatDate(new Date(), "UTC", "yyyy-MM-dd HH:mm:ss"); 
+      this.logs.push([timestamp,log]);
+    }
+  }
+
+  flush() {
+    if(this.sheet == null) {
+      this.sheet = SpreadsheetApp.getActiveSpreadsheet().insertSheet(sheetName)
+    }
+    let lastRow = this.sheet.getLastRow() + 1;
+    this.sheet.insertRowsAfter(lastRow, this.logs.length);
+    let range = this.sheet.getRange(lastRow, 1, this.logs.length, this.logs[0].length);
+    range.setValues(this.logs);
+    this.logs = [];
+  }
+}
